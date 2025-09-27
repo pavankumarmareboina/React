d94793831e0ceb81d90f62632544a6e87f248090
Pavan Kumar <pavankumarmareboina@gmail.com>
2025-09-27 20:30:53 +0530

    data

--- src/r2/d2.jsx
+++ src/r2/d2.jsx
@@ -5,27 +5,25 @@ const D2 = () => {
   const [d1, setD1] = useState([]);
 
   const API = "/data.json";
-  const f2 = async (API) => {
-    try {
-      let fRes = await fetch(API);
-      let cRes = await fRes.json();
-
-      setD1(cRes.con2);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    f2(API);
-  });
+      const datacome = async () => {
+        try {
+          const respose = await fetch(API);
+          const comeData = await respose.json();
+          setD1(comeData.con2)
+        } catch (error) {
+          console.error("Server Down...!!", error);
+        }
+      };
+    useEffect(() => {
+      datacome(API);
+    }, []);
   return (
     <>
-      <div className="flex justify-around">
+      <div className="grid grid-cols-5 text-center">
         {d1.map((it) => (
-        <div key={it.i} className="text-center rounded-2xl">
+        <div key={it.i} className="text-center border-1 p-16">
           <p>{it.p}</p>
-          <img className="w-[200px] h-[300px]" src={it.proof2} alt="" />
+          <img className="w-[200px] h-[300px] m-auto" src={it.proof2} alt="" />
           <article>{it.details2}</article>
         </div>
       ))}
