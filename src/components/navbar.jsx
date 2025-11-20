105ebf9df3ee9af3042639c1e7f628d6ea195c8f
Pavan Kumar <pavankumarmareboina@gmail.com>
2025-11-20 10:11:31 +0530

    mode toggle add

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState , useEffect } from "react";
 import { Link } from "react-router-dom";
+import { MdLightMode } from "react-icons/md";
 
 const Navbar = () => {
+  const [theme , setTheme] = useState(true)
+  const  toogleTheme = () =>{
+     setTheme((prev) => !prev);
+  }
+  useEffect(()=>{
+    if(theme){
+      document.documentElement.classList.add('modeli')
+    }
+    else{
+      document.documentElement.classList.remove('modeli')
+    }
+  })
   return(
     <nav className="bg-blue-300 h-[100px] content-center">
       <ul className="flex justify-end-safe text-[20px] gap-[32px] p-12">
         <Link to='/about'>About Page</Link>
         <Link to='/Cont'>Contact Page</Link>
         <Link to='/more'>More Page</Link>
+        <p onClick={toogleTheme}><MdLightMode /></p>
       </ul>
     </nav>
   )
